c253bf8c1c6ed93b8b6d4adc68d646e9892384c5
liupan15 <1942093769@qq.com>
2018-05-19 02:42:06 -0700

    fix map bug

--- map/main.cpp
+++ map/main.cpp
@@ -6,11 +6,16 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
-
+    //
     QWebEngineView * view = new QWebEngineView();
+    view->setUrl(QUrl("file:///home/liupan15/Desktop/database/database/map/test.html"));
+    view->show();
+
     view->setUrl(QUrl("file:///home/liupan15/Desktop/database/database/map/map.html"));
     view->show();
+    MainWindow w;
+    w.show();
+
+
     return a.exec();
 }
